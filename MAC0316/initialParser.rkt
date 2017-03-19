fc58bc4a3419738dc4e44caeaf4b9345b5b13c3c
robotenique <julianogarcia_1997@hotmail.com>
2017-03-18 22:26:09 -0300

    Add a initial file in racket language

---
+++ MAC0316/initialParser.rkt
@@ -0,0 +1,15 @@
+#lang plai-typed
+(define-type ArithC
+    [numC (n : number)]
+    [plusC (l : ArithC) (r : ArithC)]
+    [multC (l : ArithC) (r : ArithC)])
+(define (parse [s : s-expression]) : ArithC
+    (cond
+        [(s-exp-number? s) (numC (s-exp->number s))]
+        [(s-exp-list? s)
+            (let ([sl (s-exp->list s)])
+                (case (s-exp->symbol (first sl))
+                    [(+) (plusC (parse (second sl)) (parse (third sl)))]
+                    [(*) (multC (parse (second sl)) (parse (third sl)))]
+                    [else (error 'parse "INVALID LIST INPUT!")]))]
+        [else (error 'parse "INVALID INPUT!")]))
