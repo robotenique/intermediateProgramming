83725ff8d03fdd55a82398ec9f99de50975eb998
robotenique <julianogarcia_1997@hotmail.com>
2017-03-18 21:07:29 -0300

    Almost finish EP1

--- MAC0323/EP1/src/ST.java
+++ MAC0323/EP1/src/ST.java
@@ -67,9 +67,17 @@ import edu.princeton.cs.algs4.Stopwatch; // arquivo
 
 
 public class ST {
-    
+    private String [] keys;
+    private int [] values;
+    private int n = 0;
+    private int total = 0;
+    private int max;
+    private static int countDeleted;
     // cria um dicionário vazio
     public ST() {
+        keys = new String[2];
+        values = new int[2];
+        max = 2;
     }
 
     // Returns the value associated with the given key if the key is in the symbol table
@@ -79,7 +87,11 @@ public class ST {
         if (key == null) {
             throw new java.lang.NullPointerException("ST.get(): key is null");
         }
-        // escreva seu método get() aqui
+        for (int i = 0; i < n; i++)
+            if(keys[i] != null && keys[i].equals(key))
+                return values[i];
+
+        return -1;
     }
 
     // Insert the key into the symbol table and increase its value
@@ -88,7 +100,20 @@ public class ST {
         if (key == null) {
             throw new java.lang.NullPointerException("ST.put(): key is null");
         }
-        // escreva seu métod put() aqui
+        // If the key isn't in the ST
+        if(get(key) < 0) {
+            if (n == max)
+                resize(2 * n);
+            keys[n] = key;
+            values[n]++;
+            n++;
+            total++;
+        }
+        else {
+            for (int i = 0; i < n; i++)
+                if (keys[i].equals(key))
+                    values[i]++;
+        }
     }
 
     
@@ -98,7 +123,21 @@ public class ST {
         if (key == null) {
             throw new java.lang.NullPointerException("ST.delete(): key is null");
         }
-        // escreva seu método delete() aqui
+        for (int i = 0; i < n; i++) {
+            // If the number of deleted elements in greater then 3/4 of the max number of elements,
+            // It will resize down the ST.
+            if(keys[i] != null && keys[i].equals(key)) {
+                keys[i] = null;
+                values[i] = 0;
+                countDeleted++;
+                total--;
+                if(countDeleted >= 0.75*max) {
+                    countDeleted = 0;
+                    resize(max / 2 + 1);
+                }
+                return;
+            }
+        }
     }
 
     // Does this symbol table contain the given key?
@@ -107,37 +146,73 @@ public class ST {
         if (key == null) {
             throw new java.lang.NullPointerException("ST.contains(): key is null");
         }
-        // escreva seu método contains() aqui
+        for (String s : keys)
+            if (s != null && s.equals(key))
+                return true;
+
+        return false;
     }
 
     // Returns the number of key-value pairs in this symbol table
     public int size() {
-        // escreva seu métod size aqui
+        return total;
     }
 
     // Is this symbol table empty?
     // Returns: true if this symbol table is empty and false otherwise
     public boolean isEmpty() {
-        // escreva seu métod isEmpty() aqui
+        return total == 0;
     }
 
     // Returns the largest (= maior frequência) key in the symbol table
     // Throws java.util.NoSuchElementException - if the symbol table is empty
     public String max() {
-        // escreva seu métod max() aqui
+        if(isEmpty())
+            throw new java.util.NoSuchElementException("ST.max(): Symbol Table is empty");
+        int best = 0;
+        int pos = 0;
+        for (int i = 0; i < n; i++) {
+            if(values[i] > best) {
+                best = values[i];
+                pos = i;
+            }
+        }
+        return keys[pos];
     }
 
     // Returns a string representing the symbol table
     // Este string é usado quando utilizamos StdOut.print*() para exibir a tabela
     // Veja como um cliente utiliza este método no main()
     public String toString() {
-        // escreva seu método toString() aqui
+        String output = "{";
+        for (int i = 0; i < n; i++) {
+            if(keys[i] != null) {
+                output += "'" + keys[i] + "' : " + values[i];
+                if (i != n - 1)
+                    output += ",";
+            }
+
+        }
+        output += "}";
+        return output;
     }
 
 
     // move the symbol table to one of size k
     private void resize(int k) {
-        // escreva seu método resize() aqui
+        String [] newKeys = new String[k];
+        int [] newValues = new int[k];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if(keys[i] != null) {
+                newKeys[j] = keys[i];
+                newValues[j++] = values[i];
+            }
+        }
+        keys = newKeys;
+        values = newValues;
+        n = j;
+        max = k;
     }
 
 
@@ -230,9 +305,10 @@ public class ST {
         while (!in.isEmpty()) {
             // Read and return the next line.
             String linha = in.readLine();
+            StdOut.println(linha);
             String[] chaves = linha.split("\\W+");
             for (int i = 0; i < chaves.length; i++) {
-                // StdOut.print("'" + chaves[i] + "' ");
+                StdOut.print("'" + chaves[i] + "' ");
                 st2.put(chaves[i]);
             }
         }
@@ -256,9 +332,9 @@ public class ST {
                 StdOut.println("'" + st2.max() + "'");
             }
             else if (s.equals(KEYS)) {
-                for (String key: st2.keys()) {
-                    StdOut.println(key);
-                }
+                //for (String key: st2.keys()) {
+                //    StdOut.println(key);
+                //}
             }
             else {
                 // consulte o número de ocorrências de s no arquivo
