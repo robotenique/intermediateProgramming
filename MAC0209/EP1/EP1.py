2b4fb09fbb202d4d6e7856401c70e77a0a86c330
robotenique <julianogarcia_1997@hotmail.com>
2017-04-07 14:00:37 -0300

    update EP1 from MAC0209

--- MAC0209/EP1/EP1.py
+++ MAC0209/EP1/EP1.py
@@ -1,6 +1,9 @@
+import matplotlib.cm as cm
 import sys
 from itertools import islice, chain
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Number of lines for each JSON entry
 ENTRY_LENGTH = 8
@@ -30,7 +33,6 @@ class Walker:
         msr = [float(m) for m in run['measures'].split("|") if m != '']
         # Alternate measure
         if(run['mType'] == 'A'):
-            #print (sum((i/t for i, t in zip(range(5, 31, 5), msr)))/len(msr))
             return sum((i/t for i, t in zip(range(5, 31, 5), msr)))/len(msr)
         else:
             # Takes the mean of both time measures of the same pos
@@ -39,12 +41,78 @@ class Walker:
             #print (sum((i/t for i, t in zip(range(10, 31, 10), msr)))/len(msr))
             return sum((i/t for i, t in zip(range(10, 31, 10), msr)))/len(msr)
 
+    def __spaceF(self, run):
+        simVel = 30/self.__finalTime(run)
+        def f(t):
+            return simVel*t
+        return f
+
+    def __timeList(self, run):
+        msr = [float(m) for m in run['measures'].split("|") if m != '']
+        if(run['mType'] == 'N'):
+            msr = [(i+j)/2 for i, j in zip(msr[:len(msr)//2],msr[len(msr)//2:])]
+        return msr
+
+    def __spaceList(self, run):
+        t = "t"
+        if(run['mType'] == 'N'):
+            spcList = list(range(10,35, 10))
+            xticks = [t+str(i) for i in range(1,4)]
+        else:
+            spcList = list(range(5, 35, 5))
+            xticks = [t+str(i) for i in range(1,7)]
+
+
+        return [spcList, xticks]
+
+
+    def __finalTime(self, run):
+        msr = self.__timeList(run)
+        return msr[len(msr) - 1]
+
+
+
+
+
+    def plotGraph(self):
+        for run in self.times:
+            xt = self.__spaceF(run)
+            x = np.arange(0, self.__finalTime(run), 0.3)
+            y = list(map(xt, x))
+            realX = self.__timeList(run)
+            realY = self.__spaceList(run)[0]
+            timeNames = self.__spaceList(run)[1]
+            labelDict = {tValue : tName for tValue, tName in zip(realX, timeNames)}
+            xticks = [int(i) for i in np.arange(0, self.__finalTime(run), 5)]
+            xticks = sorted(xticks + list(labelDict.keys()))
+            labels = [str(i) for i in xticks]
+            labels = [labelDict.get(float(t), t) for t in labels]
+            x
+            colors = cm.rainbow(np.linspace(0, 1, len(xticks)))
+            f, axarr = plt.subplots(2)
+            axarr[0].plot(x, y)
+            for i, j in zip(list(labelDict.keys()), realY):
+                axarr[0].plot([i, i], [0, j], 'r--')
+            axarr[0].scatter(realX, realY,color='red', marker="+")
+            [t.set_color(i) for (i,t) in zip(['red']*len(labelDict), list(labelDict.values()))]
+
+            axarr[0].set_title(run["csv"])
+            axarr[0].set_xticklabels(labels)
+            axarr[0].set_xticks(xticks)
+            plt.show()
+
+            #axarr[1].scatter(x, y)
+
+
+
+
+
 '''
 def fileBlock(f, n):
-    
+
     # A generator which returns a block of 'n' lines of
     # a given file 'f', each time it is called, using iterators
-    
+
     for line in f:
         yield ''.join(chain([line], islice(f, n - 1)))
 '''
@@ -67,9 +135,11 @@ def main():
         for block in (''.join(chain([line], islice(f, ENTRY_LENGTH))) for line in f):
             w = json.loads(block)
             addWalker(w, listWalkers)
-            
+
     for walker in listWalkers:
         print(f"Mean Velocity ({walker.name} - {walker.movType}) = {walker.getVelocity():{3}.{5}} m/s")
+        walker.plotGraph()
+
 
 if __name__ == '__main__':
     main()
