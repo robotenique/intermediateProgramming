74840de7a35ca3b967f815308cf1c4fdd42f621a
robotenique <julianogarcia_1997@hotmail.com>
2017-04-12 10:24:23 -0300

    add new rerun gem

--- MAC0218/HW2/app.rb
+++ MAC0218/HW2/app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
     get '/' do
-        "<!DOCTYPE html><html><head><body><h1>HELLO WORLD!</h1></body></head></html>"
+        "<!DOCTYPE html><html><head><body><h1>BEM LOCO EMPOLGANTE!</h1></body></head></html>"
     end
 end
