902693a1013bdcf9b7df9c389884cf0b8dd8747c
robotenique <julianogarcia_1997@hotmail.com>
2017-05-12 13:30:15 -0300

    finish first part of EP08

--- MAC0323/devJava/src/PointST.java
+++ MAC0323/devJava/src/PointST.java
@@ -1,43 +1,68 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
-
+import edu.princeton.cs.algs4.RedBlackBST;
 public class PointST<Value> {
+    RedBlackBST<Point2D, Value> rbst;
     // construct an empty symbol table of points
     public PointST() {
-
+        rbst = new RedBlackBST<Point2D, Value>();
     }
     // is the symbol talbe empty?
     public boolean isEmpty() {
-        return true;
+        return rbst.isEmpty();
     }
 
     // number of points
     public int size() {
-        return 0;
+        return rbst.size();
     }
     // associate the value val with point p
-    public void put(Point2D P, Value val) {
-
+    public void put(Point2D p, Value val) {
+        if(p == null || val == null)
+            throw new java.lang.NullPointerException("NULL ARGUMENT!");
+        rbst.put(p, val);
     }
 
     // value associated with point p
     public Value get(Point2D p) {
-        return null;
+        if(p == null)
+            throw new java.lang.NullPointerException("NULL ARGUMENT!");
+        return rbst.get(p);
     }
 
     // all points in the symbol table
     public Iterable<Point2D> points() {
-        return null;
+        return rbst.keys();
     }
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if(rect == null)
+            throw new java.lang.NullPointerException("NULL ARGUMENT!");
+        Queue<Point2D> internalPoints = new Queue<>();
+        for(Point2D p : rbst.keys())
+            if(rect.contains(p))
+                internalPoints.enqueue(p);
+        return internalPoints;
     }
 
     // a nearest neighbor to point p; null if the symbol table is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        if(p == null)
+            throw new java.lang.NullPointerException("NULL ARGUMENT!");
+        if(isEmpty())
+            return null;
+        Point2D ptmp = new Point2D(0,0);
+        double minD =  Double.POSITIVE_INFINITY;
+        for(Point2D ptable : rbst.keys()) {
+            double tmpD = p.distanceSquaredTo(ptable);
+            if(tmpD < minD) {
+                minD = tmpD;
+                ptmp = ptable;
+            }
+        }
+        return ptmp;
     }
 
     // unit testing (required)
