3afa5f6205d7a82c34e81bc636a04bdc8730c8e7
robotenique <julianogarcia_1997@hotmail.com>
2017-10-03 20:05:58 -0300

    add more prolog exercises

--- MAC0444/ex4.pl
+++ MAC0444/ex4.pl
@@ -24,10 +24,13 @@ idade( barbara , 51).
 idade( maria , 79).
 irmaos( americo , paulo ).
 irmaos( carlos , sonia ).
+irmaos( daniel, stalin ).
 pai( carlos , teresa ).
 pai( daniel , americo ).
 pai( daniel , paulo ).
 pai( joaquim , daniel ).
+pai( stalin, lenin ).
+mae(sonia, quimuleke).
 mae( maria , daniel ).
 mae( barbara , ana ).
 casados( filipe , carla ).
@@ -37,3 +40,20 @@ avof(M, P) :- mae(M, X), mulher(M), pai(X, P).
 avof(M, P) :- mae(M, X), mulher(M), mae(X, P).
 avom(H, P) :- pai(H, X), homem(H), pai(X, P).
 avom(H, P) :- pai(H, X), homem(H), mae(X, P).
+bisavof(M, P) :- mulher(M), mae(M, K), avof(K, P).
+bisavof(M, P) :- mulher(M), mae(M, K), avom(K, P).
+bisavom(H, P) :- homem(H), pai(H, K), avof(K, P).
+bisavom(H, P) :- homem(H), pai(H, K), avom(K, P).
+% primo_1 is a symmetric predicate
+primo_1(P1, P2) :- P1 @< P2,  primo_1(P2, P1).
+primo_1(P1, P2) :- pai(K, P1), pai(J, P2), irmaos(K, J).
+primo_1(P1, P2) :- pai(K, P1), mae(J, P2),  irmaos(K, J).
+primo_1(P1, P2) :- mae(K, P1), pai(J, P2), irmaos(K, J).
+primo_1(P1, P2) :- mae(K, P1), mae(J, P2), irmaos(K, J).
+% primo is a symmetric predicate
+primo(A, B) :- A @< B, primo(B, A).
+primo(A, B) :- pai(PA, A), pai(PB, B), primo(PA, PB).
+primo(A, B) :- pai(PA, A), mae(MB, B), primo(PA, MB).
+primo(A, B) :- mae(MA, A), pai(PB, B), primo(MA, PB).
+primo(A, B) :- mae(MA, A), mae(MB, B), primo(MA, MB).
+primo(A, B) :- primo_1(A, B).
